746197a8ea556bc9f49516879a67bb9e3d651a05
xacrimon <joel.wejdenstal@gmail.com>
2022-01-10 12:18:59 +0100

    use mixed test file for benching

--- benches/parse.rs
+++ benches/parse.rs
@@ -4,10 +4,10 @@ use criterion::{black_box, criterion_group, criterion_main, Criterion, Throughpu
 use zaia::parser::parse;
 
 fn criterion_benchmark(c: &mut Criterion) {
-    let src = fs::read_to_string("benches/big.lua").unwrap();
+    let src = fs::read_to_string("test-files/mixed.lua").unwrap();
     let mut group = c.benchmark_group("parse");
     group.throughput(Throughput::Bytes(src.len() as u64));
-    group.bench_function("parse big.lua", |b| b.iter(|| parse(black_box(&src))));
+    group.bench_function("parse mixed.lua", |b| b.iter(|| parse(black_box(&src))));
     group.finish();
 }
 
