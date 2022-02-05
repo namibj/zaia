e8cd2dfe31e3ffd4009c686a84385f283de9971e
xacrimon <joel.wejdenstal@gmail.com>
2022-02-05 10:56:57 +0100

    scope

--- src/engine/scope.rs
+++ src/engine/scope.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use std::{alloc, collections::HashMap};
 
 use super::{
     gc::Handle,
@@ -7,23 +7,27 @@ use super::{
 use crate::error::{LuaError, LuaResult};
 
 pub struct Scope {
-    environment: Handle<Table>,
+    environment: Handle<Table<alloc::Global>>,
     stack: Vec<HashMap<String, Value>>,
 }
 
 impl Scope {
-    pub fn new(environment: Handle<Table>) -> Self {
+    pub fn new() -> Self {
         Self {
-            environment,
+            environment: Handle::unmanaged(Table::new(alloc::Global)),
             stack: Vec::new(),
         }
+    }
+
+    pub fn environment(&mut self) -> &mut Table<alloc::Global> {
+        unsafe { self.environment.get_unchecked_mut() }
     }
 
     pub fn declare(&mut self, key: String, value: Value) -> LuaResult<()> {
         if self.stack.last_mut().unwrap().insert(key, value).is_some() {
             return Err(LuaError::VariableAlreadyDeclared);
         }
-        
+
         Ok(())
     }
 
@@ -39,6 +43,16 @@ impl Scope {
             let key = Value::String(key.as_bytes().to_vec());
             self.environment.get_unchecked_mut().insert(key, value);
         }
+    }
+
+    pub fn resolve(&self, key: &str) -> Option<&Value> {
+        for scope in self.stack.iter().rev() {
+            if let Some(v) = scope.get(key) {
+                return Some(v);
+            }
+        }
+
+        unsafe { self.environment.get_unchecked().get(key.as_bytes()) }
     }
 
     pub fn push(&mut self) {
