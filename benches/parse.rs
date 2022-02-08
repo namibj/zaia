d9ea183cccaf69eb3498613497eb9149f80a5d34
Joel <joel.wejdenstal@gmail.com>
2022-02-08 07:52:04 +0000

    add bench

---
+++ benches/parse.rs
@@ -0,0 +1,19 @@
+use std::fs;
+
+use criterion::{black_box, criterion_group, criterion_main, Criterion, Throughput};
+use cstree::NodeCache;
+use zaia::parser::parse;
+
+fn criterion_benchmark(c: &mut Criterion) {
+    let src = fs::read_to_string("test-files/mixed.lua").unwrap();
+    let mut group = c.benchmark_group("parse");
+    group.throughput(Throughput::Bytes(src.len() as u64));
+    group.bench_function("parse mixed.lua", |b| {
+        b.iter(|| parse(&mut NodeCache::new(), black_box(&src)))
+    });
+    
+    group.finish();
+}
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
