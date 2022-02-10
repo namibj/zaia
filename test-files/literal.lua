9795715ef19c2582854ff9a354cf2dafda495621
xacrimon <joel.wejdenstal@gmail.com>
2022-02-10 20:22:58 +0100

    update test

--- test-files/literal.lua
+++ test-files/literal.lua
@@ -19,30 +19,3 @@ tg = {
     ["james"] = "20"
 }
 y = -18
-
-do
-    local keywords = {
-        ["and"] = true,
-        ["break"] = true,
-        ["do"] = true,
-        ["else"] = true,
-        ["elseif"] = true,
-        ["end"] = true,
-        ["false"] = true,
-        ["for"] = true,
-        ["function"] = true,
-        ["goto"] = true,
-        ["if"] = true,
-        ["in"] = true,
-        ["local"] = true,
-        ["nil"] = true,
-        ["not"] = true,
-        ["or"] = true,
-        ["repeat"] = true,
-        ["return"] = true,
-        ["then"] = true,
-        ["true"] = true,
-        ["until"] = true,
-        ["while"] = true,
-     }
-end
