86929934bf8efb58cce244887dd85e3c95898ab2
xacrimon <joel.wejdenstal@gmail.com>
2022-02-23 00:30:38 +0100

    add nan boxing trick ref

--- src/engine/value/mod.rs
+++ src/engine/value/mod.rs
@@ -4,6 +4,7 @@ use super::gc::{Handle, Trace, Visitor};
 #[cfg(not(target_endian = "little"))]
 compile_error!("zaia currently only supports little-endian platforms");
 
+// The NaN-boxing encoding used is loosely based off https://piotrduperas.com/posts/nan-boxing which is in turned based off SpiderMonkey.
 // TODO: Investigate WebKit Strategy https://brionv.com/log/2018/05/17/javascript-engine-internals-nan-boxing/
 
 const BOOL_MASK: u64 = 0x7FFE000000000002;
