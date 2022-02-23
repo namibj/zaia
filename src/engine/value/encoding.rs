7eab8c8001e307c8ef7ed1083a8b56dd9702b93a
Joel <joel.wejdenstal@gmail.com>
2022-02-23 12:57:06 +0000

    both

--- src/engine/value/encoding.rs
+++ src/engine/value/encoding.rs
@@ -1,9 +1,11 @@
 //! The NaN-boxing encoding used is loosely based off https://piotrduperas.com/posts/nan-boxing which is in turned based off SpiderMonkey.
 //! TODO: Investigate WebKit Strategy https://brionv.com/log/2018/05/17/javascript-engine-internals-nan-boxing/
 
+// TODO: Add fallback for big-endian.
 #[cfg(not(target_endian = "little"))]
 compile_error!("zaia currently only supports little-endian platforms");
 
+// TODO: Add fallback for 32-bit.
 #[cfg(not(target_pointer_width = "64"))]
 compile_error!("zaia currently only supports 64-bit platforms");
 
