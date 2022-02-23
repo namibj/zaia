9dcc3ce6715f06c13a921c25a32d9c1839d2e7a6
Joel <joel.wejdenstal@gmail.com>
2022-02-23 13:09:22 +0000

    arch support todo

--- src/engine/value/encoding.rs
+++ src/engine/value/encoding.rs
@@ -1,6 +1,7 @@
 //! The NaN-boxing encoding used is loosely based off https://piotrduperas.com/posts/nan-boxing which is in turned based off SpiderMonkey.
 //! TODO: Investigate WebKit Strategy https://brionv.com/log/2018/05/17/javascript-engine-internals-nan-boxing/
 
+// TODO: Support x86, arm, wasm32, wasm64, powerpc, powerpc64, mips, mips64
 #[cfg(not(any(target_arch = "x86_64", target_arch = "aarch64")))]
 compile_error!("zaia currently only supports x86_64 and aarch64");
 
